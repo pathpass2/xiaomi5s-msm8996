7d93d7655f346907a77bdf115a646a68e6950eea
Yassine Oudjana <y.oudjana@protonmail.com>
2022-05-09 20:02:34 +0400

    slimbus: qcom-ngd-ctrl: Register SSR notifier after all initialization steps
    The SSR notifier call can be called as soon as it is registered. This
    can cause issues as it accesses at least the ngd_up_work work struct
    and qmi_up completion. To make sure nothing uninitialized is used,
    move the SSR notifier registration to the end of probe next to PDR
    handle allocation, and initialize ngd_up_work before it is registered.
    
    Fixes: a899d324863a3 ("slimbus: qcom-ngd-ctrl: add Sub System Restart support")
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

--- drivers/slimbus/qcom-ngd-ctrl.c
+++ drivers/slimbus/qcom-ngd-ctrl.c
@@ -1501,7 +1501,6 @@ static int qcom_slim_ngd_probe(struct platform_device *pdev)
 	}
 
 	INIT_WORK(&ctrl->m_work, qcom_slim_ngd_master_worker);
-	INIT_WORK(&ctrl->ngd_up_work, qcom_slim_ngd_up_worker);
 	ctrl->mwq = create_singlethread_workqueue("ngd_master");
 	if (!ctrl->mwq) {
 		dev_err(&pdev->dev, "Failed to start master worker\n");
@@ -1548,11 +1547,6 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ctrl->nb.notifier_call = qcom_slim_ngd_ssr_notify;
-	ctrl->notifier = qcom_register_ssr_notifier("lpass", &ctrl->nb);
-	if (IS_ERR(ctrl->notifier))
-		return PTR_ERR(ctrl->notifier);
-
 	ctrl->dev = dev;
 	ctrl->framer.rootfreq = SLIM_ROOT_FREQ >> 3;
 	ctrl->framer.superfreq =
@@ -1572,6 +1566,13 @@ static int qcom_slim_ngd_ctrl_probe(struct platform_device *pdev)
 	init_completion(&ctrl->reconf);
 	init_completion(&ctrl->qmi.qmi_comp);
 	init_completion(&ctrl->qmi_up);
+
+	INIT_WORK(&ctrl->ngd_up_work, qcom_slim_ngd_up_worker);
+
+	ctrl->nb.notifier_call = qcom_slim_ngd_ssr_notify;
+	ctrl->notifier = qcom_register_ssr_notifier("lpass", &ctrl->nb);
+	if (IS_ERR(ctrl->notifier))
+		return PTR_ERR(ctrl->notifier);
 
 	ctrl->pdr = pdr_handle_alloc(slim_pd_status, ctrl);
 	if (IS_ERR(ctrl->pdr)) {
